c43925e3b443dd8a88302613b17fc7311cdb24fd
VoLis 43 <volis1990@gmail.com>
2024-08-22 18:04:51 +0500

    Kandji++

---
+++ Kandji++/Kandji++/Header.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <Windows.h>
+
+HANDLE hConsole;
+
+long int Point = 0;
+long int Error = 0;
+
+int Xnamber = 0;
+int* px = &Xnamber;
+
+
+
+
+
+
+enum ColorInConsole
+{
+	Red = 4,
+	Blue = 3
+};
+
+// void random() { random number }; int massiv[0,1,2, ...] && == mass[]; 0 = kuchi, 1 = tsuki, 2 = hito ...
+			//         0															1												2
+int Fifr[100]{0,1,2,3};
+
+std::string mass[] = { "C:/Users/volis/source/repos/Kandji++/kuchi.png", "C:/Users/volis/source/repos/Kandji++/tsuki.jpg", "C:/Users/volis/source/repos/Kandji++/hito.jpg", "/lol.png"};
+
+
+
+
+
+
---
+++ Kandji++/Kandji++/main.cpp
@@ -0,0 +1,96 @@
+#include <SFML/Graphics.hpp>
+#include "Header.h"
+
+void GetKeySearchKey(int y, std::string mass[])
+{
+	std::cout << mass << std::endl;
+}
+
+int main()
+{
+	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, Red);
+	
+	sf::RenderWindow window(sf::VideoMode(400, 400), "Picter");
+	sf::Texture Picture;
+	
+	int PointerX = 0;
+
+	int* pointer = &PointerX;
+	*pointer = 2;
+
+	std::string customString = mass[PointerX];
+
+	std::string hj = "sf";
+	std::string answer = "";
+	std::string GetNumber[] = {"fels"};
+	
+	*px = 7;
+	std::cout << " Masic X" << std::endl;
+	
+	for (std::string g : mass)
+	{
+		std::cout << g << std::endl;
+	}
+
+	std::string* Value = new std::string[*px];
+
+
+	for (int i = 0; i < *px; i++)
+	{
+		std::string fullpath = mass[Fifr[i]];
+		size_t lastSlash = fullpath.find_last_of("/\\");
+		std::string filename = fullpath.substr(lastSlash + 1);
+
+		size_t dotposition = filename.find_last_of('.');
+		
+		if (dotposition != std::string::npos)
+		{
+			filename = filename.substr(0, dotposition);
+		
+		}
+		Value[i] = filename;
+	}
+
+	for (int i = 0; i < *px; i++)
+	{
+		std::cout << Fifr[i] << " = " << Value[i] << std::endl;
+	}
+
+	std::cout << sizeof(mass) << std::endl;
+
+	Picture.loadFromFile(customString);
+
+	sf::Sprite Sprite1(Picture);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		/*for (std::string g : mass)
+		{
+			std::cout << g << std::endl;
+		}*/
+
+		window.clear(sf::Color::White);
+		window.draw(Sprite1);
+		window.display();
+
+		std::cin >> answer;
+
+		if (answer == hj)
+		{
+			std::cout << "yes, correct +1 point <3 " << std::endl;
+		}
+		else
+		{
+			std::cout << "fuck you! <3 +1 Error point" << hj << std::endl;
+		}
+
+	}
+}
